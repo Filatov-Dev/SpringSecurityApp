d5e58f15fe254d4499fe12f5716eaa32bfa8bc46
Filatov_Yurii <filatov.ios.dev@gmail.com>
2022-08-24 17:23:29 +0300

    Добавил шифрование пароля

--- src/main/java/ru/filatov/spring/springApp/services/RegistrationService.java
+++ src/main/java/ru/filatov/spring/springApp/services/RegistrationService.java
@@ -1,6 +1,8 @@
 package ru.filatov.spring.springApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.filatov.spring.springApp.models.Person;
 import ru.filatov.spring.springApp.repositories.PeopleRepository;
@@ -10,13 +12,16 @@ import javax.transaction.Transactional;
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
     @Transactional
     public void register(Person person){
+        person.setPassword(passwordEncoder.encode(person.getPassword()));// шифрование пароля пользователя
         peopleRepository.save(person);
     }
 }
